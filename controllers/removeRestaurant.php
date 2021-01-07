50fc5760ee8f7385e3b52bd9c1dab34546b5ce74
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-07 16:38:00 +0100

    Update removeRestaurant.php (AC)

--- controllers/removeRestaurant.php
+++ controllers/removeRestaurant.php
@@ -7,11 +7,12 @@
  * Located in   \admin\index.php
  */
     header("Content-Type: application/json");
-    session_start();
-    if(!isset($_SESSION["FoodistID"])) return die('{"error_code":-666,"error_message":"Access Denied"}');
+    require_once('../controllers/AccountController.php');
+    $account = new UserAccountHandler($_SESSION);
+    $account->disableUnauthorized();
     require_once('ConnectionController.php');
-
     $conn = new ConnectionHandler();
+    
     if(isset($_POST["rid"])) $conn->callQuery("DELETE FROM restaurants WHERE ID = ".$_POST["rid"]);
     else if(isset($_POST["cid"])) $conn->callQuery("DELETE FROM restaurant_accounts WHERE ID = ".$_POST["cid"]);
     $conn->finishConnection('{"success":"true"}');
