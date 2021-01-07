04a5b1f4136f9f7fe46c6e76b9fc4f66b891391c
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-07 16:37:51 +0100

    Update addNewRestaurant.php (AC)

--- controllers/addNewRestaurant.php
+++ controllers/addNewRestaurant.php
@@ -7,11 +7,12 @@
  * Located in   \admin\index.php
  */
     header("Content-Type: application/json");
-    session_start();
-    if(!isset($_SESSION["FoodistID"])) return die('{"error_code":-666,"error_message":"Access Denied"}');
+    require_once('../controllers/AccountController.php');
+    $account = new UserAccountHandler($_SESSION);
+    $account->disableUnauthorized();
     require_once('ConnectionController.php');
-
     $conn = new ConnectionHandler();
+    
     $newRestaurantName = htmlspecialchars($_POST["name"]);
     $newRestaurantAddress = htmlspecialchars($_POST["address"]);
     $newRestaurantCity = $_POST["city"];
