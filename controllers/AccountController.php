36a5d8eaca0c4ed878a0f87751203fb3c611080a
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-07 16:53:33 +0100

    Update AccountController.php

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -1,10 +1,12 @@
 <?php
-    require_once('../config/.config.inc.php');
+    require_once('../config/defines.inc.php');
     session_start();
 
     class UserAccountHandler {
         private $authorized = false;
         public $UID;
+        public $UEmail;
+        public $UAdmin;
         public $UFirstName;
         public $ULastName;
         public $UProfilePicture;
@@ -13,6 +15,8 @@
             if(isset($arr["FoodistID"])) {
                 $this->authorized = true;
                 $this->UID = $arr["FoodistID"];
+                $this->UEmail = $arr["FoodistEmail"];
+                $this->UAdmin = $arr["FoodistAdmin"];
                 $this->UFirstName = $arr["FoodistFirstName"];
                 $this->ULastName = $arr["FoodistLastName"];
                 $this->UProfilePicture = $arr["FoodistImage"];
@@ -20,8 +24,11 @@
             return 1;
         }
 
+        public function redirectAuthorized() {
+            if($this->authorized) return die(header("Location: ".SSL_APP_PATH));
+        }
         public function redirectUnauthorized() {
-            if(!($this->authorized)) return die(header("Location: ".URL_HOMEPAGE));
+            if(!($this->authorized)) return die(header("Location: ".SSL_APP_PATH));
         }
         public function disableUnauthorized() {
             if(!($this->authorized)) return die('{"error_code":-666,"error_message":"Access Denied"}');
