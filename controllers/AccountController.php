e061e3a57a2d767f285aee0c3fade4ce41483911
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-07 17:24:26 +0100

    Added fetchLogin method
    AccountController is now ready to fetching data from login page

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -6,21 +6,33 @@
         private $authorized = false;
         public $UID;
         public $UEmail;
-        public $UAdmin;
         public $UFirstName;
         public $ULastName;
         public $UProfilePicture;
+        public $UAdmin;
 
         public function __construct($arr) {
             if(isset($arr["FoodistID"])) {
                 $this->authorized = true;
                 $this->UID = $arr["FoodistID"];
                 $this->UEmail = $arr["FoodistEmail"];
-                $this->UAdmin = $arr["FoodistAdmin"];
                 $this->UFirstName = $arr["FoodistFirstName"];
                 $this->ULastName = $arr["FoodistLastName"];
                 $this->UProfilePicture = $arr["FoodistImage"];
+                $this->UAdmin = $arr["FoodistAdmin"];
             }
+            return 1;
+        }
+
+        public function fetchLogin($arr) {
+            if(!is_array($arr)) return 0;
+            $this->UID = $arr["ID"];
+            $_SESSION["FoodistID"] = $this->UID;
+            $_SESSION["FoodistEmail"] = $arr["Email"];
+            $_SESSION["FoodistFirstName"] = $arr["First_Name"];
+            $_SESSION["FoodistLastName"] = $arr["Last_Name"];
+            $_SESSION["FoodistImage"] = $arr["Image"];
+            $_SESSION["FoodistAdmin"] = $arr["Admin"];
             return 1;
         }
 
