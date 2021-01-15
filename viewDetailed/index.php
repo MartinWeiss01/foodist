ebf069d451a77f79c031955200e9d5bbc01fc6c3
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-15 21:46:45 +0100

    Optimized CSS rules

--- viewDetailed/index.php
+++ viewDetailed/index.php
@@ -56,7 +56,7 @@
                     <svg class="header-control-item" data-role="button" onclick="toggleShoppingCart()" viewBox="0 0 48 48"><path d="M 3.5 6 A 1.50015 1.50015 0 1 0 3.5 9 L 6.2558594 9 C 6.9837923 9 7.5905865 9.5029243 7.7285156 10.21875 L 8.0273438 11.78125 L 11.251953 28.716797 C 11.835068 31.772321 14.527135 34 17.638672 34 L 36.361328 34 C 39.472865 34 42.166064 31.773177 42.748047 28.716797 L 45.972656 11.78125 A 1.50015 1.50015 0 0 0 44.5 10 L 10.740234 10 L 10.675781 9.6582031 C 10.272657 7.5455321 8.4069705 6 6.2558594 6 L 3.5 6 z M 11.3125 13 L 42.6875 13 L 39.800781 28.15625 C 39.484764 29.81587 38.051791 31 36.361328 31 L 17.638672 31 C 15.948808 31 14.516781 29.8158 14.199219 28.15625 L 14.199219 28.154297 L 11.3125 13 z M 20 36 A 3 3 0 0 0 20 42 A 3 3 0 0 0 20 36 z M 34 36 A 3 3 0 0 0 34 42 A 3 3 0 0 0 34 36 z"/></svg>
 
                     <div class="menuParent">
-                        <div class="flex row hcenter account" onclick="menuHandler(this)" data-role="button">
+                        <div id="hidenseek" class="flex row hcenter account" onclick="menuHandler(this)" data-role="button">
                             <img class="accountImage" src="/images/users/<?php echo $account->UProfilePicture; ?>">
                             <span class="flex row hcenter accountDetails"><?php echo $account->DisplayName; ?> <icon>arrow_drop_down</icon></span>
                         </div>
@@ -108,47 +108,13 @@
     </body>
 
     <script>
-        const themeListenerIcon = document.querySelector("icon[theme-listener]"),
-            toastElement = document.getElementById("toast");
-
+        const themeListenerIcon = document.querySelector("icon[theme-listener]"), toastElement = document.getElementById("toast");
         window.matchMedia('(prefers-color-scheme: dark)').matches ? changeTheme(1, false) : changeTheme(0, false);
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => event.matches ? changeTheme(1) : changeTheme(0));
-
-        function changeTheme(j = -1, animations = true) {
-            let doc = document.documentElement;
-            if(animations) doc.classList.add("theme-transition");
-            if((j === 0) || (doc.hasAttribute("dark"))) {
-                doc.removeAttribute("dark");
-                showToast("[!] Switched to Light Theme.");
-                themeListenerIcon.innerText = "toggle_off";
-            } else {
-                doc.setAttribute("dark", "");
-                themeListenerIcon.innerText = "toggle_on";
-                showToast("[!] Switched to Dark Theme.");
-            }
-            if(animations) window.setTimeout(() => doc.classList.remove("theme-transition"), 1000);
-        }
-
-        function showToast(message) {
-            toastElement.innerText = message;
-            toastElement.parentElement.setAttribute("active", "");
-            window.setTimeout(() => {
-                toastElement.innerText = "";
-                toastElement.parentElement.removeAttribute("active");
-            }, 2800);
-        }
-
-        function menuHandler(caller) {
-            caller.parentElement.hasAttribute("active") ? caller.parentElement.removeAttribute("active") : caller.parentElement.setAttribute("active", "");
-        }
-
-        function hideMenu(caller) {
-            document.getElementById("menubody").style.visibility = "visible";
-            caller.parentElement.removeAttribute("active");
-            window.setTimeout(() => {
-                document.getElementById("menubody").style.visibility = "hidden";
-            }, 300);
-        }
+        function changeTheme(j = -1, animations = true) {let doc = document.documentElement;if(animations) doc.classList.add("theme-transition");if((j === 0) || (doc.hasAttribute("dark"))) {doc.removeAttribute("dark");themeListenerIcon.innerText = "toggle_off";} else {doc.setAttribute("dark", "");themeListenerIcon.innerText = "toggle_on";}if(animations) window.setTimeout(() => doc.classList.remove("theme-transition"), 1000);}
+        function showToast(message) {toastElement.innerText = message;toastElement.parentElement.setAttribute("active", "");window.setTimeout(() => {toastElement.innerText = "";toastElement.parentElement.removeAttribute("active");}, 2800);}
+        function menuHandler(caller) {caller.parentElement.hasAttribute("active") ? caller.parentElement.removeAttribute("active") : caller.parentElement.setAttribute("active", "");}
+        function hideMenu(caller) {document.getElementById("menubody").style.visibility = "visible";caller.parentElement.removeAttribute("active");window.setTimeout(() => {document.getElementById("menubody").style.visibility = "hidden";}, 300);}
 
         document.addEventListener("DOMContentLoaded", () => {checkCart();});
         const shoppingCartBox = document.getElementById("shoppingCart");
