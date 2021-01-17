168c1432b45d38000527a7ac8536fb40e26ff75f
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-17 14:32:11 +0100

    Update defines.inc.php

--- config/defines.inc.php
+++ config/defines.inc.php
@@ -1,4 +1,3 @@
 <?php
+    define('APP_ORIGIN', 'https://foodist.store');
     define('SSL_APP_PATH', 'https://foodist.store/');
-    define('AUTO_APP_PATH', '//foodist.store/');
-    define('REL_PATH', '/');
