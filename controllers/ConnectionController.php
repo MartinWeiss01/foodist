7bb51f6d4278c9d2e33785cedf1a5477880ca1e6
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-17 14:38:11 +0100

    Update ConnectionController.php

--- controllers/ConnectionController.php
+++ controllers/ConnectionController.php
@@ -1,22 +1,23 @@
 <?php
+    define('TRUSTED', true);
     require_once(dirname(__DIR__).'/config/.config.inc.php');
 
     class ConnectionHandler {
         public $connection;
         public function __construct() {
             $this->connection = new mysqli(SQL_SERVER, SQL_USER, SQL_PASS, SQL_DB);
-            if($this->connection->connect_error) $this->exitScript('{"error_code":-1,"error_message":"Connection Error"}');
+            if($this->connection->connect_error) $this->exitScript('{"error_code":-308,"error_message":"Connection Error"}');
             $this->connection->set_charset("utf8");
             return $this->connection;
         }
 
         public function callQuery($query) {
-            $result = $this->connection->query($query) or $this->exitScript('{"error_code":-2,"error_message":"Invalid Query"}');
+            $result = $this->connection->query($query) or $this->finishConnection('{"error_code":-309,"error_message":"Invalid Query"}');
             return $result;
         }
 
         public function callMultiQuery($query) {
-            $result = $this->connection->multi_query($query) or $this->exitScript('{"error_code":-2,"error_message":"Invalid Query"}');
+            $result = $this->connection->multi_query($query) or $this->finishConnection('{"error_code":-310,"error_message":"Invalid Query"}');
             return $result;
         }
 
