b9f7944dd126f4e8cc7b441da5843231aa3f0614
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-01-17 11:36:57 +0100

    Create placeOrder.php

---
+++ controllers/placeOrder.php
@@ -0,0 +1,29 @@
+<?php
+/* placeOrder.php
+ * Method: POST
+ * Parameters: {}
+ * (c) 2020 Martin Weiss, martinweiss.cz
+ * -----------------
+ * Located in   \viewDetailed\index.php
+ */
+    header("Content-Type: application/json");
+    require_once('../controllers/AccountController.php');
+    $account = new UserAccountHandler($_SESSION);
+    require_once('ConnectionController.php');
+    $conn = new ConnectionHandler();
+    
+    if(!$account->UCart) $conn->finishConnection('{"response_message":"Empty cart"}');
+    else {
+        $conn->callQuery("INSERT INTO orders (userID) VALUES ($account->UID)");
+        $orderID = $conn->connection->insert_id;
+        $queryString = "";
+        foreach($_SESSION["FoodistCart"] as $val => $item) {
+            $queryString .= "INSERT INTO order_food (orderID, foodID, foodPrice, itemCount) VALUES ($orderID, $item[0], $item[2], $item[1]); ";
+        }
+        $conn->callMultiQuery($queryString);
+        $account->UCart = array();
+        $account->updateUserCart();
+    }
+
+    $conn->finishConnection('{"response_message":"Passed through"}');
+?>
