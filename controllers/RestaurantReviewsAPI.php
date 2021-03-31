98cbe989fa269b81fbb61ef44933acb81ca28ee6
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2021-03-31 15:12:00 +0200

    Create RestaurantReviewsAPI.php

---
+++ controllers/RestaurantReviewsAPI.php
@@ -0,0 +1,35 @@
+<?php
+/* RestaurantReviewsAPI.php
+ * Method: POST
+ * Parameters: {rid}
+ * (c) 2021 Martin Weiss, martinweiss.cz
+ * -----------------
+ * Located in   \restaurant\index.php
+ */
+    header("Content-Type: application/json");
+    require_once(__DIR__.'/AccountController.php');
+    $account = new UserAccountHandler($_SESSION);
+    $account->disableDirect($_SERVER);
+    require_once(__DIR__.'/ConnectionController.php');
+    $conn = new ConnectionHandler();
+
+    $rid = $conn->escape($_POST['rid']);
+    $conn->prepare("SELECT reviews.ID, reviews.userID, users.First_Name, reviews.stars, reviews.comment, reviews.Posted FROM reviews LEFT JOIN users ON users.ID = reviews.userID WHERE reviews.restaurantID = ?", "i", $rid);
+    $result = $conn->execute();
+    $json = array("total" => 0, "reviews" => array());
+
+    $json["total"] = $result->num_rows;
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $arr = array();
+            $arr += ["ID" => $row['ID']];
+            $arr += ["UID" => $row['userID']];
+            $arr += ["Name" => ($row['First_Name'] ? $row['First_Name'] : "Uživatel")];
+            $arr += ["Stars" => $row['stars']];
+            $arr += ["Comment" => $row['comment']];
+            $arr += ["Date" => $row['Posted']];
+            array_push($json["reviews"], $arr);
+        }
+    }
+    $conn->finishConnection(json_encode($json));
+?>
